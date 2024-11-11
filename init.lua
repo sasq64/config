169c77a2772c6149ae8118f399603051bd8bac9a
Jonas Minnberg <sasq64@gmail.com>
2024-11-11 08:09:53 +0100

    Surround plugin

--- init.lua
+++ init.lua
@@ -822,6 +822,16 @@ require("lazy").setup({
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = { signs = false },
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
