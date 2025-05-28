ec112f364ec76ddc766b8b8efae1467e0ffaacbf
Jonas Minnberg <sasq64@gmail.com>
2025-05-28 15:33:08 +0200

    Added fish config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+zoxide init fish | source
+starship init fish | source
+source ~/.venv/bin/activate.fish
+export EDITOR=nvim
+alias cdp "cd $(git rev-parse --show-toplevel)"
