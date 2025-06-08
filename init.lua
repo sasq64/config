bff4f7fd0b169d6a4a23e932db1e20a54994bd3b
Jonas Minnberg <sasq64@gmail.com>
2025-06-08 20:46:40 +0200

    colors

--- init.lua
+++ init.lua
@@ -833,21 +833,20 @@ require("lazy").setup({
 			update_interval = 1000,
 		},
 	},
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd.hi("Comment guifg=#e03080")
-		end,
-	},
+	-- { -- You can easily change to a different colorscheme.
+	-- 	-- Change the name of the colorscheme plugin below, and then
+	-- 	-- change the command in the config to whatever the name of that colorscheme is.
+	-- 	--
+	-- 	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	init = function()
+	-- 		-- Load the colorscheme here.
+	-- 		-- Like many other themes, this one has different styles, and you could load
+	-- 		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+	-- 		vim.cmd.hi("Comment guifg=#e03080")
+	-- 	end,
+	-- },
 
 	-- Highlight todo, notes, etc in comments
 	{
@@ -1210,6 +1209,15 @@ local files_gitdir = function()
 		})
 	end
 end
+
+vim.cmd.colorscheme("PaperColor")
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.cmd.hi("Comment guifg=#e03080")
+	end,
+})
+vim.cmd.hi("Comment guifg=#e03080")
 
 vim.opt.swapfile = false
 vim.opt.lbr = true
